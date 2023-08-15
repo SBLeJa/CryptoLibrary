522105df310d34285f9cbb9251f0605032d4e9c8
jlehmika <jani.lehmikangas@sebitti.fi>
2023-08-15 09:08:03 +0300

    Initial Commit

---
+++ Sources/CryptoLibrary/CryptoLibrary.swift
@@ -0,0 +1,6 @@
+public struct CryptoLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CryptoLibraryTests/CryptoLibraryTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CryptoLibrary
+
+final class CryptoLibraryTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CryptoLibrary().text, "Hello, World!")
+    }
+}
